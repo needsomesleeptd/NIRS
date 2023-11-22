175d4b66694a23a2f377969538ae1b4dbb760db7
Andrew Razin <razin.andrew.ru@mail.ru>
2023-11-22 19:10:08 +0300

    fixed main.py

--- image_annotation/scripts/main.py
+++ image_annotation/scripts/main.py
@@ -4,15 +4,14 @@ import os
 from PIL import Image
 
 
-def extract_images_from_pdf(pdf_path, output_folder, dpi=180): #make dpi lower if low
+def extract_images_from_pdf(pdf_path, output_folder, prefix, dpi=180):  # make dpi lower if low
     pdf_document = fitz.open(pdf_path)
 
     for page_number in range(pdf_document.page_count):
         page = pdf_document[page_number]
-        image = page.get_pixmap(matrix=fitz.Matrix(dpi/72, dpi/72),alpha=False)
+        image = page.get_pixmap(matrix=fitz.Matrix(dpi / 72, dpi / 72), alpha=False)
         pil_image = Image.frombytes("RGB", [image.width, image.height], image.samples)
-        pil_image.save(f"{output_folder}/page_{page_number + 1}.png", dpi=(dpi, dpi))
-
+        pil_image.save(f"{output_folder}/{prefix}_{page_number + 1}.png", dpi=(dpi, dpi))
 
     pdf_document.close()
 
@@ -23,7 +22,7 @@ output_format = "png"
 
 reports = os.listdir(path_to_pdfs)
 for report in reports:
-    output_dir = os.path.join(path_to_images,report.removesuffix('.pdf'))
-    #print(report)
-    os.makedirs(output_dir)
-    extract_images_from_pdf(path_to_pdfs + report, output_dir)
+    output_dir = os.path.join(path_to_images, report.removesuffix('.pdf'))
+    # print(report)
+    os.makedirs(output_dir,exist_ok=True)
+    extract_images_from_pdf(path_to_pdfs + report, output_dir,report)
